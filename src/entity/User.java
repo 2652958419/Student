840ffae5a25c7612804d3cb0d85b07de0bf511ca
童先生 <@192.168.254.1>
2020-03-02 16:14:40 +0800

    第一次提交

---
+++ src/entity/User.java
@@ -0,0 +1,32 @@
+package entity;
+/**
+ * 用户类
+ * @author 田先生
+ *
+ */
+public class User {
+	private String name;//姓名
+	private int age;//年龄
+	
+	public User() {
+		super();
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public User(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	
+}
---
+++ src/service/impl/UserService.java
@@ -0,0 +1,13 @@
+package service.impl;
+
+import entity.User;
+
+public class UserService implements service.UserService {
+
+	@Override
+	public void save(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
